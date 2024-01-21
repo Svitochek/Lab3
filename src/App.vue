82b7ca1353319922efbb5f19ba825aebfbb61b9c
Svitochek <bluewitch130@gmail.com>
2024-01-21 22:14:33 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <button @click="currentView = 'Population'">Население</button>
+    <button @click="currentView = 'Emissions'">Экология</button>
+
+    <Population v-if="currentView === 'Population'" />
+    <Emissions v-if="currentView === 'Emissions'" />
+  </div>
+</template>
+
+<script>
+import Population from './components/Population.vue';
+import Emissions from './components/Emissions.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Population,
+    Emissions
+  },
+  data() {
+    return {
+      currentView: 'Population'
+    };
+  }
+};
+</script>
---
+++ src/components/Emissions.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Выбросы парниковых газов</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Emissions' 
+}
+</script>
